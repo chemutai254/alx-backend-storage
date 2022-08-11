18e484d5c279a402cf81f2ed7e02bcfac5285dcb
chemutai254 <nancy.chemutai254>
2022-08-11 13:27:05 -0700

    In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL,
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT UC_users UNIQUE(ID, email)
 );
