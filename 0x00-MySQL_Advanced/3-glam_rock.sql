6556e631b160e26cdc515a5390669d5a0a470788
chemutai254 <nancy.chemutai254>
2022-08-11 14:04:36 -0700

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- SQL Scripts that list all bands
-SELECT band_name, IFNULL(split,2020) - IFNULL(formed,0) AS lifespan
+SELECT band_name,
+    IFNULL(split,2020) - IFNULL(formed,0) AS lifespan
 FROM metal_bands
 WHERE style like '%Glam rockGlam rock%'
 ORDER BY 2 DESC;
