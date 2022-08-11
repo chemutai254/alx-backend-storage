6fe8acfb026efae173db22c6c2f940cd58a71bd6
chemutai254 <nancy.chemutai254>
2022-08-11 14:35:02 -0700

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Create triggers that resets the attribute when it changes
+CREATE trigger reset_valid 
+before update ON users
+for each row 
+BEGIN 
+    if NEW.email <> OLD.email 
+    THEN SET NEW.valid_email = 0;
+    end if;
+END // 
+delimiter;
