86f4ade10c40f31b0028965b3b0360fc8e3acd3c
chemutai254 <nancy.chemutai254>
2022-08-11 02:57:12 -0700

    We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- SQL Script that creates table users
+-- With attributes: id,email, name
+CREATE TABLE IF NOT EXISTS users (
+ id INT NOT NULL AUTO_INCREMENT,
+ email VARCHAR(255) NOT NULL,
+ name VARCHAR(255),
+ PRIMARY KEY (id),
+ CONSTRAINT UC_users UNIQUE(ID, email)
+);
